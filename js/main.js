88a8a4c23b7aae265aa3f794a1ba3a3802c4eabd
elielson71 <elielsonboavista@gmail.com>
2021-03-07 10:04:44 -0300

    tudo ok

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function show_register() {
+    let contin = document.querySelector(".instagram-continue");
+    let register = document.querySelector('.register');
+
+    contin.style.display = "none";
+    register.style.display = 'flex';
+}
